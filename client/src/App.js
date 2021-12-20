e97177eb1ecb6d53486002106c7239ef4f13e397
abdumamdouh <eabdogfx@gmail.com>
2021-12-20 19:13:17 +0200

    edit the App Component

--- client/src/App.js
+++ client/src/App.js
@@ -6,27 +6,8 @@ import {
 } from "react-router-dom";
 
 // pages
-import {
-  Home,
-  Shirts,
-  Shoes,
-  HeadPhones,
-  Contact,
-  Cart,
-  AllProducts,
-  SingleProduct,
-  Login,
-  PageNotFound404
-} from "./pages";
 
 // components
-import Navbar from "./components/Navbar/Navbar";
-import CartSidebar from "./components/CartSidebar/CartSidebar";
-import Sidebar from "./components/Sidebar/Sidebar";
-import Footer from "./components/Footer/Footer";
-
-// scroll to top component
-import ScrollToTop from "./utils/ScrollToTop";
 
 // protected route
 import ProtectedRoute from "./ProtectedRoute";
@@ -34,10 +15,6 @@ import ProtectedRoute from "./ProtectedRoute";
 const App = () => {
   return (
     <Router>
-      <ScrollToTop />
-      <Navbar />
-      <CartSidebar />
-      <Sidebar />
       <Switch>
         <ProtectedRoute exact path="/" component={Home} name="Home Page" />
         <Route exact path="/login" component={Login} name="Login Page" />
@@ -47,38 +24,6 @@ const App = () => {
           component={Shirts}
           name="Shirts Page"
         />
-        <ProtectedRoute
-          exact
-          path="/shoes"
-          component={Shoes}
-          name="Shoes Page"
-        />
-        <ProtectedRoute
-          exact
-          path="/headphones"
-          component={HeadPhones}
-          name="HeadPhones Page"
-        />
-        <ProtectedRoute
-          exact
-          path="/account"
-          component={Contact}
-          name="Contact Page"
-        />
-        <ProtectedRoute exact path="/cart" component={Cart} name="Cart Page" />
-        <ProtectedRoute
-          exact
-          path="/products"
-          component={AllProducts}
-          name="AllProducts Page"
-        />
-        <ProtectedRoute
-          exact
-          path="/products/:id"
-          component={SingleProduct}
-          name="SingleProduct Page"
-        />
-
         <Route
           exact
           path="/404"
