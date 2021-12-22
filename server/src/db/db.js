d4bc588df588440ffd2f47bdb840f081946382c2
MarwanEmadMourad <marwanemad36@gmail.com>
2021-12-22 16:35:59 +0200

    Creating Applicant db-model.

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,8 +1,20 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('GP-db','root','root',{
+const db = new Sequelize('gp-db','root','root',{
     dialect: 'mysql',
     host:'localhost'
  });
 
- module.exoprts = sequelize ; 
+// Uncomment the following sector to test the db connection
+
+// const testConnection = async() => {
+//      try {
+//     await db.authenticate();
+//     console.log('Connection has been established successfully.');
+//     } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+//     }
+// }
+// testConnection()
+
+module.exports = db ; 
--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const db = require('./db/db')
 
-
 const app = express();
 const PORT = 3000;
 
 app.use(express.json()) 
 
+
+db.sync().then( res => {
+}).catch( error =>{
+    // console.log(error)
+}) 
 
 app.listen(PORT , () =>{
     console.log('Server is up on port '+ PORT)
