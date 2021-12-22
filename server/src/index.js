d3b20cff20fb3d02b8cad3d512b4f530c43463f3
MarwanEmadMourad <marwanemad36@gmail.com>
2021-12-22 15:03:31 +0200

    Starting our express app and connecting to the local db.

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,13 @@
-//index
+const express = require('express')
+const db = require('./db/db')
+
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json()) 
+
+
+app.listen(PORT , () =>{
+    console.log('Server is up on port '+ PORT)
+})
