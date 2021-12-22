ca4ca0b74eab5eef6d5c6218cbdea423bd90612f
salmaehab <salmaiihab@gmail.com>
2021-12-22 22:38:45 +0200

    fixing errors

--- client/src/redux/rootReducer.js
+++ client/src/redux/rootReducer.js
@@ -2,9 +2,9 @@ import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
-import productsReducer from "./reducers/products";
-import sidebarReducer from "./reducers/sideBar";
-import userReducer from "./reducers/user";
+// import productsReducer from "./reducers/products";
+// import sidebarReducer from "./reducers/sideBar";
+// import userReducer from "./reducers/user";
 
 const persistConfig = {
   key: "root",
@@ -13,9 +13,9 @@ const persistConfig = {
 };
 
 const rootReducer = combineReducers({
-  products: productsReducer,
-  sidebar: sidebarReducer,
-  user: userReducer
+  // products: productsReducer,
+  // sidebar: sidebarReducer,
+  // user: userReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
