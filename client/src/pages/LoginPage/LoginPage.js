f3c91192ddfb4ae872fb68c18531650956aa149b
salmaehab <salmaiihab@gmail.com>
2021-12-22 22:46:32 +0200

    Login page

---
+++ client/src/pages/LoginPage/LoginPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import AccountBox from "../../components/layout/AccountBox/AccountBox";
+function LoginPage() {
+  return <AccountBox />;
+}
+
+export default LoginPage;
