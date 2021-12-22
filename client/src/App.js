3d519379c2b0938b77915085093277ed0288ea0c
salmaehab <salmaiihab@gmail.com>
2021-12-22 22:32:47 +0200

    adding routes to navigation bar

--- client/src/App.js
+++ client/src/App.js
@@ -7,18 +7,38 @@ import {
 
 // pages
 
+import HomePage from "./pages/HomePage/HomePage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import Applicant from "./pages/SignUpPage/Applicant";
+import Recruiter from "./pages/SignUpPage/Recruiter";
 // components
 import HelloWorld from "./components/HelloWorld";
-
+import Layout from "./components/layout/Layout";
 // protected route
 import ProtectedRoute from "./ProtectedRoute";
 
 const App = () => {
   return (
     <Router>
+     <Layout>
       <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <Route path="/login" exact>
+          <LoginPage />
+        </Route>
+        <Route path="/signup-applicant" exact>
+          <Applicant />
+        </Route>
+        <Route path="/signup-recruiter" exact>
+          <Recruiter/>
+        </Route>
+      </Switch>
+    </Layout>
+      {/* <Switch>
         <ProtectedRoute exact path="/" component={HelloWorld} name="" />
-        <Route exact path="" component={} name="" />
+        <Route exact path="" component={HelloWorld} name="" />
         <ProtectedRoute
           exact
           path=""
@@ -33,7 +53,7 @@ const App = () => {
         />
         <Redirect to="/404" />
       </Switch>
-      <Footer />
+      <Footer /> */}
     </Router>
   );
 };
