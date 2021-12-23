0281458f4f1d8094de6913a407c124888dfbf664
AntoineZakaria <tonyzakaria53@gmail.com>
2021-12-23 12:47:58 +0200

    somestyle not all

---
+++ client/src/components/layout/AccountBox/SignupBox.js
@@ -0,0 +1,21 @@
+import React from "react";
+import classes from "./AccountBox.module.scss";
+import Register from "../../Forms/SignupForm"
+function Signup() {
+  return (
+    <div className={classes.BoxContainer2}>
+      <div className={classes.TopContainer}>
+        <div className={classes.BoxDrop}></div>
+      <div className={classes.HeaderContainer}>
+          <h2>Welcome !</h2>
+          <h5>sign up and join us!</h5>
+      </div>
+      </div>
+      <div className={classes.InnerContainer}>
+          <Register />
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
