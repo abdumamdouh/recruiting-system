052ff718ddb8b8a4ceeb81686fbe95ec899e830d
salmaehab <salmaiihab@gmail.com>
2021-12-23 21:37:38 +0200

    Top section of home page

---
+++ client/src/pages/HomePage/TopSection.js
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+import { useHistory } from "react-router-dom";
+
+import BackgroundImg from "../../assets/images/company_team.jpg";
+import { Logo } from "../../components/Logo/Logo";
+import { Marginer } from "../../components/Marginer/Marginer";
+import { Button } from "../../components/Button/Button";
+const TopContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  padding: 0;
+  background-image: url(${BackgroundImg});
+`;
+
+const BackgroundFilter = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(55, 55, 55, 0.89);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const MotivitionalText = styled.h2`
+  font-size: 33px;
+  font-weight: bold;
+  margin: 0;
+  color: #fff;
+`;
+export function TopSection(props) {
+  const history = useHistory();
+  const handleApplicant = () => {
+    history.push("/signup-applicant");
+  };
+  const handleRecruiter = () => {
+    history.push("/signup-recruiter");
+  };
+  return (
+    <TopContainer>
+      <BackgroundFilter>
+        <Marginer direction="vertical" margin="5em" />
+        <Logo />
+        <Marginer direction="vertical" margin="2em" />
+        <MotivitionalText>Find your next opportunity</MotivitionalText>
+        <Marginer direction="vertical" margin="2em" />
+        <Button onClick={handleApplicant}>Apply as an Applicant</Button>
+        <Marginer direction="vertical" margin="1em" />
+        <Button onClick={handleRecruiter}>Apply as a Recruiter</Button>
+      </BackgroundFilter>
+    </TopContainer>
+  );
+}
