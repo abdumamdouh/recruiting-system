2b11858edf2982b457be32b75af5964faecfe66d
salmaehab <salmaiihab@gmail.com>
2021-12-24 23:27:11 +0200

    update login form

--- client/src/components/Forms/LoginForm.js
+++ client/src/components/Forms/LoginForm.js
@@ -1,28 +1,54 @@
 import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 import classes from "./common.module.scss";
+
 function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+
+  const handleLoginForm = e => {
+    e.preventDefault();
+    console.log(email, password);
+  };
   return (
     <div className={classes.BoxContainer}>
-      <div className={classes.FormContainer}>
-        <input type="email" placeholder="Email" className={classes.Input} />
+      <form onSubmit={handleLoginForm}>
+        <input
+          type="email"
+          placeholder="Email"
+          className={classes.Input}
+          value={email}
+          onChange={e => {
+            setEmail(e.target.value);
+          }}
+        />
         <input
           type="password"
           placeholder="Password"
           className={classes.Input}
+          value={password}
+          onChange={e => {
+            setPassword(e.target.value);
+          }}
         />
-      </div>
+        <button type="submit" className={classes.SubmitButton}>
+          Sign in
+        </button>
+      </form>
       <div style={{ margin: "10px" }}>
         <a className={classes.MutedLink} href="#">
           Forgot your password?
         </a>
       </div>
-      <button type="submit" className={classes.SubmitButton}>
-        Sign in
-      </button>
+
       <div style={{ margin: "10px" }}>
         <a className={classes.MutedLink} href="#">
           Don't have an account?
-          <a href="#" className={classes.BoldLink}>SignUp</a>
+          <a href="#" className={classes.BoldLink}>
+            SignUp
+          </a>
         </a>
       </div>
     </div>
