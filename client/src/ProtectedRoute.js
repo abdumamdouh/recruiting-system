b2c137e3227af1777fa7e6daea5e75cbd907d62f
abdumamdouh <eabdogfx@gmail.com>
2021-12-25 21:35:41 +0200

    reformat of the protect route

--- client/src/ProtectedRoute.js
+++ client/src/ProtectedRoute.js
@@ -4,32 +4,32 @@ import { Redirect, Route } from "react-router";
 import { useSelector } from "react-redux";
 
 const ProtectedRoute = ({ component: Component, exact, path }) => {
-  const { } = useSelector((state) => state);
+    const {} = useSelector((state) => state);
 
-  return (
-    <Route
-      exact={exact}
-      path={path}
-      render={(props) =>
-        // this.props.authedUser
-        //TODO: add condition for Protected Route
-        true ? (
-          <Component {...props} />
-        ) : (
-          <Redirect
-            // to property pass info to be used in withRouter props
-            // we will pass from state represnts the location of the current route
-            to={{
-              pathname: "/login",
-              state: {
-                from: props.location
-              }
-            }}
-          />
-        )
-      }
-    />
-  );
+    return (
+        <Route
+            exact={exact}
+            path={path}
+            render={(props) =>
+                // this.props.authedUser
+                //TODO: add condition for Protected Route
+                true ? (
+                    <Component {...props} />
+                ) : (
+                    <Redirect
+                        // to property pass info to be used in withRouter props
+                        // we will pass from state represnts the location of the current route
+                        to={{
+                            pathname: "/login",
+                            state: {
+                                from: props.location
+                            }
+                        }}
+                    />
+                )
+            }
+        />
+    );
 };
 
 export default ProtectedRoute;
