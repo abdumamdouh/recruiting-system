4d3c21b7a7e92fc9a10e6253f44bdbd411bc9a13
abdumamdouh <eabdogfx@gmail.com>
2021-12-25 21:32:23 +0200

    make the 404 button redirect to the homepage

--- client/src/pages/PageNotFound404.js
+++ client/src/pages/PageNotFound404.js
@@ -3,40 +3,40 @@ import { Button, Container } from "@material-ui/core";
 
 import PageNotFoundGif from "../assets/images/404.gif";
 
-const PageNotFound404 = () => {
-  return (
-    <Container className="da">
-      <div
-        style={{
-          margin: "auto",
-          width: "50%",
-          padding: "10px"
-        }}
-      >
-        <img src={PageNotFoundGif} alt="pageNotFound" />
-      </div>
+const PageNotFound404 = (props) => {
+    return (
+        <Container className="da">
+            <div
+                style={{
+                    margin: "auto",
+                    width: "50%",
+                    padding: "10px"
+                }}
+            >
+                <img src={PageNotFoundGif} alt="pageNotFound" />
+            </div>
 
-      <div
-        style={{
-          margin: "auto",
-          width: "50%",
-          padding: "10px"
-        }}
-      >
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="secondary"
-          onClick={() => {
-            this.props.history.push("/");
-          }}
-        >
-          Back
-        </Button>
-      </div>
-    </Container>
-  );
+            <div
+                style={{
+                    margin: "auto",
+                    width: "50%",
+                    padding: "10px"
+                }}
+            >
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="secondary"
+                    onClick={() => {
+                        props.history.push("/");
+                    }}
+                >
+                    Back
+                </Button>
+            </div>
+        </Container>
+    );
 };
 
 export default PageNotFound404;
