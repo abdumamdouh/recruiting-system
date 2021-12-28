7c119300e35e883bb328035e96b61dc844ee2535
MarwanEmadMourad <marwanemad36@gmail.com>
2021-12-28 13:40:01 +0200

    Ensuring unique emails when signing-up.

--- server/src/controller/routes/Applicant.js
+++ server/src/controller/routes/Applicant.js
@@ -9,9 +9,15 @@ const router = new express.Router()
 router.post('/Applicant/Sign-up' , async (req,res) =>{
     const applicant = req.body
     try {
-        const record = await Applicant.create( applicant )
-        const token = await record.generateAuthToken()
-        res.status(200).send({token,record})   
+        const takenEmail = await Applicant.findOne({ where: { email:req.body.email } }) || await Recruiter.findOne({ where: { email:req.body.email } })
+        
+        if (takenEmail){
+            throw new Error("Email is already registered.")
+        } else {
+            const record = await Applicant.create( applicant )
+            const token = await record.generateAuthToken()
+            res.status(200).send({token,message:"Registered Successfully."})   
+        }
     } catch (error) {
         res.status(400).send(error.message)
     }
