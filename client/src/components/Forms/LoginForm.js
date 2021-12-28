594a8cb336b7a9f21b8c8f6dc8b4fabaee2f622f
salmaehab <salmaiihab@gmail.com>
2021-12-28 17:48:36 +0200

    add redirect to redux

--- client/src/components/Forms/LoginForm.js
+++ client/src/components/Forms/LoginForm.js
@@ -1,26 +1,49 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import Button from "../Button/Button";
 import classes from "./common.module.scss";
 import { loginUserAction } from "../../redux/actions/user";
 function LoginForm(props) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState({});
+    const [apiError, setApiError] = useState("");
     const dispatch = useDispatch();
+    const user = useSelector(state => state.user);
+    const validate = (email, password) => {
+        const error = {};
+        if (!password) error.password = "Password is required";
 
-    console.log(dispatch);
+        if (!email) error.email = "Email is required";
+        else if (
+            !/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+                email
+            )
+        ) {
+            error.email = "Invalid Email";
+        }
+        setErrors(error);
 
-    const handleLoginForm = (e) => {
-        e.preventDefault();
-        dispatch(loginUserAction(email, password));
-
+        return error;
+    };
+    const showError = () => {
+        setApiError("Email or password is incorrect.");
+    };
+    const redirect = () => {
         const fromObj = props.location.state || {
             from: { pathname: "/" }
         };
 
         const path = fromObj.from.pathname;
         props.history.push(path);
+    };
+
+    const handleLoginForm = e => {
+        e.preventDefault();
+        const errors = validate(email, password);
+        dispatch(loginUserAction(email, password, redirect, showError));
     };
     return (
         <div className={classes.BoxContainer}>
@@ -30,19 +53,28 @@ function LoginForm(props) {
                     placeholder="Email"
                     className={classes.Input}
                     value={email}
-                    onChange={(e) => {
+                    onChange={e => {
                         setEmail(e.target.value);
                     }}
                 />
+                {errors.email && (
+                    <label className={classes.error}>{errors.email}</label>
+                )}
                 <input
                     type="password"
                     placeholder="Password"
                     className={classes.Input}
                     value={password}
-                    onChange={(e) => {
+                    onChange={e => {
                         setPassword(e.target.value);
                     }}
                 />
+                {errors.password && (
+                    <label className={classes.error}>{errors.password}</label>
+                )}
+                {errors === null && (
+                    <label className={classes.error}>{apiError}</label>
+                )}
                 <button type="submit" className={classes.SubmitButton}>
                     Sign in
                 </button>
@@ -56,7 +88,7 @@ function LoginForm(props) {
             <div style={{ margin: "10px" }}>
                 <a className={classes.MutedLink} href="#">
                     Don't have an account?
-                    <a href="#" className={classes.BoldLink}>
+                    <a href="#" className={classes.BoldLink} onClick={redirect}>
                         SignUp
                     </a>
                 </a>
--- client/src/redux/actions/user.js
+++ client/src/redux/actions/user.js
@@ -1,113 +1,114 @@
 import axios from "axios";
 import {
-  REGISTER_APPLICANT_REQUEST,
-  REGISTER_APPLICANT_SUCCESS,
-  REGISTER_APPLICANT_FAIL,
-  REGISTER_RECRUITER_REQUEST,
-  REGISTER_RECRUITER_SUCCESS,
-  REGISTER_RECRUITER_FAIL,
-  LOGIN_REQUEST,
-  LOGIN_SUCCESS,
-  LOGIN_FAIL,
-  LOGOUT
+    REGISTER_APPLICANT_REQUEST,
+    REGISTER_APPLICANT_SUCCESS,
+    REGISTER_APPLICANT_FAIL,
+    REGISTER_RECRUITER_REQUEST,
+    REGISTER_RECRUITER_SUCCESS,
+    REGISTER_RECRUITER_FAIL,
+    LOGIN_REQUEST,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT
 } from "../types";
 
 const serverURL = "http://localhost:5000";
 
 // register Applicant
-const registerApplicantAction = (Applicant,redirect,showError) => {
-  console.log(Applicant);
-  return async dispatch => {
-    try {
-      dispatch({ type: REGISTER_APPLICANT_REQUEST });
-      const config = {
-        headers: {
-          "Content-Type": "application/json"
+const registerApplicantAction = (Applicant, redirect, showError) => {
+    console.log(Applicant);
+    return async dispatch => {
+        try {
+            dispatch({ type: REGISTER_APPLICANT_REQUEST });
+            const config = {
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            };
+            const { data } = await axios.post(
+                `${serverURL}/Applicant/Sign-up`,
+                Applicant,
+                config
+            );
+            dispatch({ type: REGISTER_APPLICANT_SUCCESS, payload: data });
+            redirect();
+        } catch (error) {
+            dispatch({
+                type: REGISTER_APPLICANT_FAIL,
+                payload: error.response && error.response.data
+            });
+            showError();
         }
-      };
-      const { data } = await axios.post(
-        `${serverURL}/Applicant/Sign-up`,
-        Applicant,
-        config
-      );
-      dispatch({ type: REGISTER_APPLICANT_SUCCESS, payload: data });
-      redirect();
-    } catch (error) {
-      dispatch({
-        type: REGISTER_APPLICANT_FAIL,
-        payload: error.response && error.response.data
-      });
-      showError();
-    }
-  };
+    };
 };
 
 // register Recruiter
-const registerRecruiterAction = (Recruiter,redirect,showError) => {
-  return async dispatch => {
-    try {
-      dispatch({ type: REGISTER_RECRUITER_REQUEST });
-      const config = {
-        headers: {
-          "Content-Type": "application/json"
+const registerRecruiterAction = (Recruiter, redirect, showError) => {
+    return async dispatch => {
+        try {
+            dispatch({ type: REGISTER_RECRUITER_REQUEST });
+            const config = {
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            };
+            const { data } = await axios.post(
+                `${serverURL}/Recruiter/Sign-up`,
+                Recruiter,
+                config
+            );
+            dispatch({ type: REGISTER_RECRUITER_SUCCESS, payload: data });
+            redirect();
+        } catch (error) {
+            dispatch({
+                type: REGISTER_RECRUITER_FAIL,
+                payload: error.response && error.response.data
+            });
+            showError();
         }
-      };
-      const { data } = await axios.post(
-        `${serverURL}/Recruiter/Sign-up`,
-        Recruiter,
-        config
-      );
-      dispatch({ type: REGISTER_RECRUITER_SUCCESS, payload: data });
-      redirect()
-    } catch (error) {
-      dispatch({
-        type: REGISTER_RECRUITER_FAIL,
-        payload: error.response && error.response.data
-        
-      });
-      showError();
-    }
-  };
+    };
 };
 
-const loginUserAction = (email, password) => {
-  return async dispatch => {
-    try {
-      dispatch({ type: LOGIN_REQUEST });
-      const config = {
-        headers: {
-          "Content-Type": "application/json"
-        }
-      };
+const loginUserAction = (email, password, redirect, showError) => {
+    return async dispatch => {
+        try {
+            dispatch({ type: LOGIN_REQUEST });
+            const config = {
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            };
 
-      const { data } = await axios.post(
-        `${serverURL}/login`,
-        {
-          email,
-          password
-        },
-        config
-      );
-      dispatch({ type: LOGIN_SUCCESS, payload: data });
-    } catch (error) {
-      dispatch({
-        type: LOGIN_FAIL,
-        payload: error.response && error.response.data.message
-      });
-    }
-  };
+            const { data } = await axios.post(
+                `${serverURL}/login`,
+                {
+                    email,
+                    password
+                },
+                config
+            );
+            dispatch({ type: LOGIN_SUCCESS, payload: data });
+            redirect();
+        } catch (error) {
+            dispatch({
+                type: LOGIN_FAIL,
+                payload: error.response && error.response.data
+            });
+            showError();
+        }
+    };
 };
 
 //Log out
 const logoutUserAction = () => {
-  return {
-    type: LOGOUT
-  };
+    return {
+        type: LOGOUT
+    };
 };
 
 export {
-  registerApplicantAction,
-  registerRecruiterAction,
-  loginUserAction,
-  logoutUserAction
+    registerApplicantAction,
+    registerRecruiterAction,
+    loginUserAction,
+    logoutUserAction
 };
