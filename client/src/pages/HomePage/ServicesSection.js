c9dd046d2899e7a60aea47dd0424d08c1bd80d06
abdumamdouh <eabdogfx@gmail.com>
2021-12-28 23:30:41 +0200

    main description - first look

--- client/src/pages/HomePage/ServicesSection.js
+++ client/src/pages/HomePage/ServicesSection.js
@@ -16,25 +16,22 @@ const ServicesContainer = styled(Element)`
     flex-direction: column;
     align-items: center;
     padding: 10px 0;
+    backgroundcolor: #354054;
 `;
 
 export function ServicesSection(props) {
     return (
         <ServicesContainer name="servicesSection">
-            <SectionTitle>Best Quality Software</SectionTitle>
+            <SectionTitle>Automation Hiring Process</SectionTitle>
             <Marginer direction="vertical" margin="3em" />
             <OurSerivce
-                title="Fully integrated services"
-                description="We build and deliver fully integrated webapp
-                with customized control panels that fit your 
-                company needs"
+                title="Fully automated services"
+                description="We provide several plugin modules for the recruiter to automate or customize all the hiring pipeline based on how the recruiter wants to evaluate the applied candidates."
                 imgUrl={Service1Img}
             />
             <OurSerivce
-                title="Mobile optimized"
-                description="We build and deliver fully integrated webapp
-                with customized control panels that fit your 
-                company needs"
+                title="Job posting and searching"
+                description="We offer the ability to post your job as a recruiter of search for jobs as an applicants with customized control panels that fit your needs"
                 imgUrl={Service2Img}
                 isReversed
             />
@@ -42,7 +39,7 @@ export function ServicesSection(props) {
                 title="Quality is our priority"
                 description="We have teams of professional developers, designers
                 and managers that ensures delivering the best 
-                software quality for your company"
+                automation hiring process for your company"
                 imgUrl={Service3Img}
             />
         </ServicesContainer>
