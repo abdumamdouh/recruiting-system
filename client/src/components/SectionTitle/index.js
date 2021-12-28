91be2b12b3913ca49dcc5b6932deeba6240bc7f6
abdumamdouh <eabdogfx@gmail.com>
2021-12-28 23:13:43 +0200

    vertical margin to the title

--- client/src/components/SectionTitle/index.js
+++ client/src/components/SectionTitle/index.js
@@ -4,6 +4,7 @@ export const SectionTitle = styled.h1`
     font-size: 34px;
     font-weight: bold;
     color: #000;
+    margin: 30px 0;
 
     @media screen and (max-width: 480px) {
         text-align: center;
