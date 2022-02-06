5e58db85944d6673dd46e2a4cfdb63cf3176136f
AntoineZakaria <tonyzakaria53@gmail.com>
2022-02-06 19:37:18 +0200

    topsection

--- client/src/pages/HomePage/TopSection.js
+++ client/src/pages/HomePage/TopSection.js
@@ -9,7 +9,6 @@ import { Logo } from "../../components/Logo/Logo";
 import { Marginer } from "../../components/Marginer/Marginer";
 import { Button } from "../../components/Button/Button";
 import { DownArrow } from "../../components/DownArrow/DownArrow";
-import EditProfile from "../../components/profile/EditProfile";
 const TopContainer = styled.div`
     width: 100%;
     height: 90vh;
@@ -62,8 +61,7 @@ export function TopSection(props) {
             <TopContainer>
                 <BackgroundFilter>
                     <Marginer direction="vertical" margin="3em" />
-                    <Logo />
-                    <EditProfile/>
+                    <Logo />         
                     <Marginer direction="vertical" margin="1em" />
                     <MotivitionalText>
                         To make the hiring process easier and more organized
