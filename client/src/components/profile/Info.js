ebbd0e90d492d70d4063f69278da20a4be2ec322
AntoineZakaria <tonyzakaria53@gmail.com>
2022-02-06 19:37:36 +0200

    profile page

--- client/src/components/profile/Info.js
+++ client/src/components/profile/Info.js
@@ -1,12 +1,9 @@
 import React, { useState, useEffect } from 'react'
-import Avatar from '../Avatar'
 import EditProfile from './EditProfile'
-import FollowBtn from '../FollowBtn'
-import Followers from './Followers'
-import Following from './Following'
-import { GLOBALTYPES } from '../../redux/actions/globalTypes'
+import { useDispatch, useSelector } from "react-redux";
 
-const Info = ({id, auth, profile, dispatch}) => {
+
+const Info = () => {
     
     const [userData, setUserData] = useState([])
     const [onEdit, setOnEdit] = useState(false)
@@ -14,43 +11,26 @@ const Info = ({id, auth, profile, dispatch}) => {
     const [showFollowers, setShowFollowers] = useState(false)
     const [showFollowing, setShowFollowing] = useState(false)
 
-    useEffect(() => {
-        if(id === auth.user._id){
-            setUserData([auth.user])
-        }else{
-            const newData = profile.users.filter(user => user._id === id)
-            setUserData(newData)
-        }
-    }, [id, auth, dispatch, profile.users])
+    const user = useSelector(state => state.user);
+    const{record}=user.userInfo;
 
-
-    useEffect(() => {
-        if(showFollowers || showFollowing || onEdit){
-            dispatch({ type: GLOBALTYPES.MODAL, payload: true})
-        }else{
-            dispatch({ type: GLOBALTYPES.MODAL, payload: false})
-        }
-    },[showFollowers, showFollowing, onEdit, dispatch])
-    
-
+ 
     return (
         <div className="info">
             {
-                userData.map(user => (
-                    <div className="info_container" key={user._id}>
-                        <Avatar src={user.avatar} size="supper-avatar" />
+                    <div className="info_container" >
 
                         <div className="info_content">
                             <div className="info_content_title">
-                                <h2>{user.username}</h2>
+                                <h2>{record.firstName}</h2>
                                 {
-                                    user._id === auth.user._id
-                                    ?  <button className="btn btn-outline-info"
+                                   
+                                    <button className="btn btn-outline-info"
                                     onClick={() => setOnEdit(true)}>
                                         Edit Profile
                                     </button>
                                     
-                                    : <FollowBtn user={user} />
+                                   
                                 }
                                 
                                 
@@ -58,42 +38,30 @@ const Info = ({id, auth, profile, dispatch}) => {
 
                             <div className="follow_btn">
                                 <span className="mr-4" onClick={() => setShowFollowers(true)}>
-                                    {user.followers.length} Followers
+                                    Followers
                                 </span>
                                 <span className="ml-4" onClick={() => setShowFollowing(true)}>
-                                    {user.following.length} Following
+                                     Following
                                 </span>
                             </div>
 
-                            <h6>{user.fullname} <span className="text-danger">{user.mobile}</span></h6>
-                            <p className="m-0">{user.address}</p>
-                            <h6 className="m-0">{user.email}</h6>
-                            <a href={user.website} target="_blank" rel="noreferrer">
-                                {user.website}
+                            <h6>{record.firstName}{record.lastName}<span className="text-danger">012</span></h6>
+                            <p className="m-0">{record.position}</p>
+                            <h6 className="m-0">{record.email}</h6>
+                            <a href={"sssss"} target="_blank" rel="noreferrer">
+                                website
                             </a>
-                            <p>{user.story}</p>
+                            <p>story</p>
                         </div>
 
                         {
                             onEdit && <EditProfile setOnEdit={setOnEdit} />
                         }
 
-                        {
-                            showFollowers &&
-                            <Followers 
-                            users={user.followers} 
-                            setShowFollowers={setShowFollowers} 
-                            />
-                        }
-                        {
-                            showFollowing &&
-                            <Following 
-                            users={user.following} 
-                            setShowFollowing={setShowFollowing} 
-                            />
-                        }
+                      
+                     
                     </div>
-                ))
+                
             }
         </div>
     )
