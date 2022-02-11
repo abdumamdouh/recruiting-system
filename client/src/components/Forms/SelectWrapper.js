c7d6317826ac9413d8a2621825db09e4a1ea18f6
salmaehab <salmaiihab@gmail.com>
2022-02-11 15:50:47 +0200

    helper form components

---
+++ client/src/components/Forms/SelectWrapper.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { TextField, MenuItem } from '@material-ui/core';
+import { useField, useFormikContext } from 'formik';
+const SelectWrapper = ({
+    name,options,
+    ...otherProps
+}) => {
+    const { setFieldValue } = useFormikContext();
+    const [field, meta] = useField(name)
+    const handleChange = e => {
+    setFieldValue(name, e.target.value)
+    }
+    const configSelect = {
+        ...field,
+        ...otherProps,
+        select: true,
+        variant: 'outlined',
+        fullWidth: true,
+        onChange: handleChange
+      };
+      if (meta && meta.touched && meta.error) {
+        configSelect.error = true;
+        configSelect.helperText = meta.error;
+      }
+    
+    return (
+        <TextField {...configSelect}>
+              {options.map((option) => {
+        return (
+          <MenuItem key={option.value} value={option}>
+            {option.label}
+          </MenuItem>
+        )
+      })}
+        </TextField>
+    );
+}
+
+export default SelectWrapper;
