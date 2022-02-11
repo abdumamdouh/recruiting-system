34f84b6645c5055ffe63c7faa455efc439661b8c
AntoineZakaria <tonyzakaria53@gmail.com>
2022-02-11 15:07:36 +0200

    adding many jobPosts

--- client/src/components/Feed/FeedPage.js
+++ client/src/components/Feed/FeedPage.js
@@ -1,13 +1,21 @@
 import React from "react";
 
 import JobPost from "./JobPost";
+import classes from "./Feed.module.scss"
+
 
 
 
 const Feed = () => {
 
+    const array = [1,2,3,4,5]
+
     return(
-        <div>
+        <div>{
+            array.map(job=>(
+                <JobPost/>
+            ))
+            }
             <JobPost/>
         </div>
     )
