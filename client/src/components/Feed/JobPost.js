d4e23097c7a71be5d37d82b37dea289a03f87eba
AntoineZakaria <tonyzakaria53@gmail.com>
2022-02-11 14:02:23 +0200

    adding ui to JobPost

--- client/src/components/Feed/JobPost.js
+++ client/src/components/Feed/JobPost.js
@@ -7,9 +7,43 @@ import React from "react";
 const JobPost = () => {
 
     return(
-        <div>
-            job Post
+        <div className='job'>
+            <img
+            src={'./19152370.jpg'}
+            alt='logo'
+            className='image'/>
+            <div className='job-info'>
+                <div>
+                    <span>job title</span>
+                    <div>
+                        time
+                    </div>
+                    
+                    <button>
+                            test
+                    </button>
+                    <p>
+
+                    this is a job
+                    </p>
+
+                </div>
+                
+                <div className='job-icons'>
+                    
+                    
+
+                </div>
+
+
+                    
+                
+
+            </div>
+
+
         </div>
+
     )
 
 }
