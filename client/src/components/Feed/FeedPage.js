1c08d928ffefca4ba126872ac1ad8ec8247bea2b
AntoineZakaria <tonyzakaria53@gmail.com>
2022-02-11 15:32:56 +0200

    rendering many posts

--- client/src/components/Feed/FeedPage.js
+++ client/src/components/Feed/FeedPage.js
@@ -8,7 +8,7 @@ import classes from "./Feed.module.scss"
 
 const Feed = () => {
 
-    const array = [1,2,3,4,5]
+    const array = [1,2,3,4,5.6,7,8,9,10]
 
     return(
         <div className={classes.list}>{
