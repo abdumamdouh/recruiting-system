47500f5d2d9f4f013be08c9707d14b5d436e0682
abdumamdouh <eabdogfx@gmail.com>
2022-02-13 17:56:22 +0200

    add plain js object for job

--- client/src/pages/JobPage/JobPage.js
+++ client/src/pages/JobPage/JobPage.js
@@ -1,6 +1,16 @@
 import React from "react";
 import Job from "../../components/Job/Job";
 
+//TODO: replace the jobObject with the data from BE
+const jobObject = {
+    description: "Backend Engineer",
+    workPlaceType: "On Site",
+    employmentType: "Full Time",
+    title: "Engineer",
+    yearsOfExperience: 3,
+    careerLevel: "junior"
+};
+
 export default function JobPage() {
     return (
         <>
