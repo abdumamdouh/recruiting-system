032bda232468ad0013e05baaf77ba7ec82281faf
salmaehab <salmaiihab@gmail.com>
2022-02-14 13:34:23 +0200

    add loading skeleton

--- client/src/components/Feed/FeedPage.js
+++ client/src/components/Feed/FeedPage.js
@@ -1,78 +1,61 @@
 import React, { useState, useEffect } from "react";
-import {useDispatch, useSelector} from 'react-redux'
+import { useDispatch, useSelector } from "react-redux";
+import Skeleton from "@mui/material/Skeleton";
+import Stack from "@mui/material/Stack";
 import JobPost from "./JobPost";
-import classes from "./Feed.module.scss"
-import ReactPaginate from "react-paginate"
-import {getJobsAction} from '../../redux/actions/jobs'
-
+import classes from "./Feed.module.scss";
+import ReactPaginate from "react-paginate";
+import { getJobsAction } from "../../redux/actions/jobs";
 
 const Feed = () => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(getJobsAction(1))
-  }, [dispatch]);
-    
-    const Jobs = useSelector(state=> state.jobs.Jobs)
-    const {Count} = useSelector(state=>state.jobs)
+    useEffect(() => {
+        dispatch(getJobsAction(1));
+    }, [dispatch]);
+
+    const Jobs = useSelector(state => state.jobs.Jobs);
+    const { Count } = useSelector(state => state.jobs);
     //console.log('cc',Count)
-   // const Count = 0;
+    // const Count = 0;
 
+    const [pageNumber, setPageNumber] = useState(1);
 
-    
+    // const pagesVisited = pageNumber*jobsPerPage
 
-    const[pageNumber,setPageNumber]=useState(1)
-
-
-   // const pagesVisited = pageNumber*jobsPerPage
-
-    const displayJobs = ()=>{
-
-    }
-    const changePage = ({selected})=>{
-        setPageNumber(selected+1);
-       dispatch(getJobsAction(selected+1))
-    }
-    if (Jobs!==undefined){
-    return(
-          
-            <div className={classes.list}>{
-              
-                        Jobs.map(job=>(
-            <JobPost job={job}/>
-                        ))
-                        }
-                        <br />
-                        <ReactPaginate
-                        previousLabel={"Previous"}
-                        nextLabel={"Next"}
-                        pageCount={Math.ceil(Count/10)}
-                        onPageChange={changePage}
-                        containerClassName={classes.paginationBttns}
-                        previousLinkClassName={classes.previousBttn}
-                        nextLinkClassName={classes.nextBttn}
-                        disabledClassName={classes.paginationDisabled}
-                        activeClassName={classes.paginationActive} />
-                    
-
+    const displayJobs = () => {};
+    const changePage = ({ selected }) => {
+        setPageNumber(selected + 1);
+        dispatch(getJobsAction(selected + 1));
+    };
+    if (Jobs !== undefined) {
+        return (
+            <div className={classes.list}>
+                {Jobs.map(job => (
+                    <JobPost job={job} />
+                ))}
+                <br />
+                <ReactPaginate
+                    previousLabel={"Previous"}
+                    nextLabel={"Next"}
+                    pageCount={Math.ceil(Count / 10)}
+                    onPageChange={changePage}
+                    containerClassName={classes.paginationBttns}
+                    previousLinkClassName={classes.previousBttn}
+                    nextLinkClassName={classes.nextBttn}
+                    disabledClassName={classes.paginationDisabled}
+                    activeClassName={classes.paginationActive}
+                />
             </div>
+        );
+    } else
+        return (
+            <Stack spacing={1}>
+                <Skeleton variant="text" />
+                <Skeleton variant="rectangular" width={210} height={210} />
+                <Skeleton variant="rectangular" width={210} height={210} />
+            </Stack>
+        );
+};
 
-                      
-                      
-                 
-       
-                        
-    )
-    
-  }
-  else return(
-    <div>
-      loading.....
-    </div>
-  )
-
-
-}
-
-
-export default Feed
+export default Feed;
