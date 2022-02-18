cd6e0ea68c644f11603f6647fb444e909eda34d3
MarwanEmadMourad <marwanemad36@gmail.com>
2022-02-18 15:23:34 +0200

    getting the login route as it was.

--- server/src/controller/routes/General.js
+++ server/src/controller/routes/General.js
@@ -1,5 +1,4 @@
 const express = require("express");
-// const { JSON } = require("sequelize/dist");
 const Applicant = require("../../models/Applicant");
 const Recruiter = require("../../models/Recruiter");
 const RecOrApp = require("../middleware/RecOrApp");
@@ -7,19 +6,10 @@ const RecOrApp = require("../middleware/RecOrApp");
 const router = new express.Router();
 
 // login for recruiters or applicants
-router.post("/Login", async (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-
-    try {        const record =
-            (await Recruiter.findByCredentials(email, password)) ||
-            (await Applicant.findByCredentials(email, password));
-        if (record) {
-            const token = await record.generateAuthToken();
-            res.status(200).send({ token, record });
-        } else {
-            throw new Error("Email or password is incorrect.");
-
+router.post('/Login' , async (req,res) =>{
+    const email = req.body.email 
+    const password = req.body.password
+    try {
         const recruiter = await Recruiter.findByCredentials(email,password)
         const applicant = await Applicant.findByCredentials(email,password)
         if (recruiter){
@@ -29,7 +19,7 @@ router.post("/Login", async (req, res) => {
                 name:`${recruiter.firstName} ${recruiter.lastName}.`
             })
         } else if (applicant){
-            const token = await applicant.generateAuthToken() 
+            const token = await applicant.generateAuthToken()
             res.status(200).send({token,
                 type:"Applicant",
                 name:`${applicant.firstName} ${applicant.lastName}.`
@@ -37,40 +27,25 @@ router.post("/Login", async (req, res) => {
         } 
         else {
             throw new Error('Email or password is incorrect.')
-
-        }}
+        }
     } catch (error) {
-        res.status(400).send(error.message);
+        res.status(400).send(error.message)
     }
-});
+})
 
 // logout for recruiters or applicants
 router.post("/logout", RecOrApp, async (req, res) => {
     try {
         let user = req.recruiter ? req.recruiter : req.applicant;
         let tokens = JSON.parse(user.tokens);
-        // console.log("current token", req.token);
-        // console.log("list of tokens before filter", JSON.parse(user.tokens));
         tokens = tokens.filter((token) => token.token !== req.token);
-        // console.log("list of tokens after filter", tokens);
         user.tokens = JSON.stringify(tokens);
         await user.save();
-        res.send();
+        res.send("Logged out successfully");
     } catch (error) {
         res.status(500).send(error.message);
     }
 });
-// get profile by id
-router.get("/profile/:id", RecOrApp, async (req, res) => {
-    try {
-        const user = req.recruiter ? Recruiter : Applicant;
-        const profile = await user.findByPk(req.params.id);
-        if (!profile) {
-            return res.status(404).send();
-        }
-        res.send(profile);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
+
+
 module.exports = router;
