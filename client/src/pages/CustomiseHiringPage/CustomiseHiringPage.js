935d99102f501f909a5a3f53c8dad58e4df047a1
abdumamdouh <eabdogfx@gmail.com>
2022-02-21 16:21:02 +0200

    fix import

--- client/src/pages/CustomiseHiringPage/CustomiseHiringPage.js
+++ client/src/pages/CustomiseHiringPage/CustomiseHiringPage.js
@@ -1,9 +1,35 @@
 import React from "react";
 
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import CssBaseline from "@mui/material/CssBaseline";
+
 export default function CustomiseHiringPage() {
     return (
         <>
-            <div>Helllllo</div>
+            <Container
+                component="main"
+                maxWidth="sm"
+                style={{ border: "1px solid", borderRadius: "15px" }}
+                sx={{ overflow: "auto" }}
+            >
+                <CssBaseline />
+
+                <Box
+                    sx={{
+                        marginTop: 5,
+                        marginBottom: 5,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "left"
+                    }}
+                >
+                    <Typography variant="h5" color="black">
+                        Customise Your Hiring Pipeline
+                    </Typography>
+                </Box>
+            </Container>
         </>
     );
 }
