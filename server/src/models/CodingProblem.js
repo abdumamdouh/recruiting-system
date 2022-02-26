3880789678138b59f396c8a27f2cba65afb3e160
AntoineZakaria <tonyzakaria53@gmail.com>
2022-02-26 10:17:55 +0200

    Adding coding problem model

--- server/src/models/CodingProblem.js
+++ server/src/models/CodingProblem.js
@@ -1,2 +1,23 @@
 const Sequelize = require('sequelize')
 const db = require('../db/db')
+
+
+const CodingProblemBank = db.define('CodingProblemBank',{
+    id:{
+        type:Sequelize.INTEGER,
+        primaryKey:true,
+        autoIncrement:true,
+        allowNull:false
+
+    },
+    description:{
+        type:Sequelize.STRING,
+        allowNull:false
+    },
+    timeConstraints:{
+        type:Sequelize.STRING,
+    },
+    memoryConstraints:{
+        type:Sequelize.STRING,
+    }
+});
