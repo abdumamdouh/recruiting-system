ac0975fc86a798e321da138132a9ee9f4b256b56
AntoineZakaria <tonyzakaria53@gmail.com>
2022-02-26 10:39:00 +0200

    adding testcases model

---
+++ server/src/models/TestCases.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize')
+const db = require('../db/db')
+const CodingProblemBank = require("./CodingProblemBank")
+
+
+const TestCases = db.define('TestCases',{
+    id:{
+        type:Sequelize.INTEGER,
+        primaryKey:true,
+        autoIncrement:true,
+        allowNull:false
+
+    },
+    codingProblemId:{
+        type:Sequelize.INTEGER,
+        allowNull:false,
+        references: {
+            model: 'CodingProblemBank', 
+            key: 'id'
+        },
+    },
+    inputs:{
+        type:Sequelize.JSON,
+        allowNull:false
+    },
+    outputs:{
+        type:Sequelize.JSON,
+        allowNull:false
+
+    }
+});
+
+
+
+module.exports=TestCases;
