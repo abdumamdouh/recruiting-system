57ee4fd37b8ada59ecb184741faf97dcb42a01b2
salmaehab <salmaiihab@gmail.com>
2022-02-27 21:51:59 +0200

    getting avatar from job

--- client/src/components/Feed/JobPost.js
+++ client/src/components/Feed/JobPost.js
@@ -6,56 +6,19 @@ import Button from "@mui/material/Button";
 
 const JobPost = ({job}) => {
     const history = useHistory()
-    const [photo,setPhoto]= useState()
-    const [imgUrl, setImgUrl] = useState();
-    const state = useSelector((state) => state);
-    const fetchData = async()=> {
-        try {
-            const rawResponse = await fetch(
-                'http://localhost:5000/Recruiter/me/avatar',
-                {
-                    method: "GET",
-                    headers: {
-                        // Accept: "application/json",
-                        // "Content-Type": "data:image/jpeg;base64",
-                        Authorization: "Bearer " + state.user.userInfo.token
-                    }
-                }
-            );
-            const data = await rawResponse.blob();
-            const reader = new FileReader();
-            reader.readAsDataURL(data);
-            reader.onloadend = () => {
-              const base64data = reader.result;
-              setImgUrl(base64data);
-            };
-            console.log(data);
-            setPhoto(data);
-        } catch (error) {
-            console.log(error.message);
-        }
-    }
-    useEffect(() => {
-        fetchData();
-    }, []);
+    const avatar =job.Recruiter.avatar;
+    const base64String = btoa(String.fromCharCode(...new Uint8Array(avatar.data)));
     const handleRedirect = ()=>{
         history.push(`/feed/job/${job.id}`);
     }
     //console.log(job)
     return(
         <div className={classes.job} onClick={handleRedirect}>
-            {/* <img
-            src={'./mentor.png'}
-            alt='logo'
-            className={classes.image}/> */}
-            {/* <img
-            src={`data:image/jpeg;base64,${photo}`}
-            alt='logo'
-            className={classes.image}/> */}
             <img
-            src={imgUrl} 
+            src={`data:image/jpeg;base64,${base64String}`}
             alt='logo'
             className={classes.image}/>
+    
             <div className={classes.jobInfo}>
                 <div>
                     <span>{job.title}</span>
