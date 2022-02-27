292edd4cd2c96fbf4ca505fa8c06bce1f729a044
abdumamdouh <eabdogfx@gmail.com>
2022-02-27 16:51:26 +0200

    MCQ object for testing and header of the mcq

--- client/src/pages/McqTestPage/McqTestPage.js
+++ client/src/pages/McqTestPage/McqTestPage.js
@@ -8,6 +8,63 @@ import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 
+const MCQ = {
+    jobId: 1,
+    topic: "ana ba7b rouby",
+    questions: [
+        {
+            question: "sdasjjkvdasdad",
+            choices: [
+                "ssdsavdvdvdd",
+                "jfhjdadjhsad",
+                "ghsdhsdagh",
+                "utruhfreife"
+            ],
+            id: 1
+        },
+        {
+            question: "dsanvkddsaffdk",
+            choices: [
+                "skdfksd",
+                "jjdsfhksfdhksdh",
+                "jdfhjbfdbvk",
+                "kfdjkjgdflgkdlf"
+            ],
+            id: 2
+        },
+        {
+            question: "to be or not to be",
+            choices: ["be", "not to be", "be be", "el7"],
+            id: 3
+        },
+        {
+            question: "1+1",
+            choices: ["2", "11", "'2'", "3"],
+            id: 4
+        },
+        {
+            question: "arza3",
+            choices: ["arza3", "arza3", "arza3", "arza3"],
+            id: 5
+        },
+        {
+            question: "el7",
+            choices: ["el7", "el7", "el7", "el7"],
+            id: 6
+        },
+        {
+            question: "alllllllo",
+            choices: [
+                "aloooooooooo",
+                "aloooooooooo",
+                "aloooooooooo",
+                "aloooooooooo"
+            ],
+            id: 7
+        }
+    ]
+};
+
 const McqTestPage = (props) => {
     //ID of the job
     const { ID } = useParams();
@@ -28,7 +85,13 @@ const McqTestPage = (props) => {
                     alignItems: "left"
                 }}
             >
-                <h2>Hello</h2>
+                <Typography
+                    color="black"
+                    variant="h5"
+                    style={{ textAlign: "center" }}
+                >
+                    {MCQ.topic}
+                </Typography>
             </Box>
         </Container>
     );
