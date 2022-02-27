33c3d9a538b0bab243dca1dfa296baa949f87398
abdumamdouh <eabdogfx@gmail.com>
2022-02-27 16:43:03 +0200

    md container with basic layout

--- client/src/pages/McqTestPage/McqTestPage.js
+++ client/src/pages/McqTestPage/McqTestPage.js
@@ -1,10 +1,36 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Divider from "@mui/material/Divider";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
 
 const McqTestPage = (props) => {
+    //ID of the job
+    const { ID } = useParams();
     return (
-        <div>
-            <h2>Hello</h2>
-        </div>
+        <Container
+            component="main"
+            maxWidth="md"
+            style={{ border: "1px solid", borderRadius: "15px" }}
+            sx={{ overflow: "auto" }}
+        >
+            <CssBaseline />
+            <Box
+                sx={{
+                    marginTop: 5,
+                    marginBottom: 5,
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "left"
+                }}
+            >
+                <h2>Hello</h2>
+            </Box>
+        </Container>
     );
 };
 
