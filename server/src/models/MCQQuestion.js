6d7ee87c12818d23ab9bcd725c4f4e47a381c673
Marwan-Alghandour <marwanalghandour23@gmail.com>
2022-02-28 03:10:43 +0200

    Add alias to model Question as questions

--- server/src/models/MCQQuestion.js
+++ server/src/models/MCQQuestion.js
@@ -6,7 +6,7 @@ const Question = require("./Question");
 const MCQQuestion = db.define("MCQQuestion", {}, { freezeTableName: true });
 
 // relation between MCQ nad question ( M-->N )
-MCQ.belongsToMany(Question, { through: MCQQuestion });
-Question.belongsToMany(MCQ, { through: MCQQuestion });
+MCQ.belongsToMany(Question, { as: "questions", through: MCQQuestion });
+Question.belongsToMany(MCQ, { as: "questions", through: MCQQuestion });
 
 module.exports = MCQQuestion;
