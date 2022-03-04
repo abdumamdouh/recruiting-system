4b8492a750cc1ac5a35b65e33fa091cc897e9b7f
Marwan-Alghandour <marwanalghandour23@gmail.com>
2022-03-05 01:19:15 +0200

    Change score typr to decimal

--- server/src/models/MCQStat.js
+++ server/src/models/MCQStat.js
@@ -5,7 +5,7 @@ const MCQStat = db.define(
     "MCQStat",
     {
         score: {
-            type: Sequelize.INTEGER(3),
+            type: Sequelize.DECIMAL(4, 2),
             allowNull: false
         },
         applicantId: {
