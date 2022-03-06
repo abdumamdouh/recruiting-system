5d900be86fdbab25b8e7a487d2a75f92930c69a8
Marwan-Alghandour <marwanalghandour23@gmail.com>
2022-03-06 18:50:45 +0200

    adding overlay for popup

--- client/src/components/modal/Message.js
+++ client/src/components/modal/Message.js
@@ -2,38 +2,40 @@ import React from "react";
 import "./modal.scss";
 
 function Message({ setOpenModal, message }) {
-  return (
-    <div className="modalBackground">
-      <div className="modalContainer">
-        <div className="titleCloseBtn">
-          <button
-            onClick={() => {
-              setOpenModal(false);
-            }}
-          >
-            x
-          </button>
-        </div>
-        {/* <div className="title">
+    return (
+        <div className="overlay">
+            <div className="modalBackground">
+                <div className="modalContainer">
+                    <div className="titleCloseBtn">
+                        <button
+                            onClick={() => {
+                                setOpenModal(false);
+                            }}
+                        >
+                            x
+                        </button>
+                    </div>
+                    {/* <div className="title">
           <h3>Are You Sure You Want to Continue?</h3>
         </div> */}
-        <div className="body">
-          <p>{message}</p>
+                    <div className="body">
+                        <p>{message}</p>
+                    </div>
+                    <div className="footer">
+                        <button
+                            onClick={() => {
+                                setOpenModal(false);
+                            }}
+                            id="cancelBtn"
+                        >
+                            close
+                        </button>
+                        {/* <button>Continue</button> */}
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="footer">
-          <button
-            onClick={() => {
-              setOpenModal(false);
-            }}
-            id="cancelBtn"
-          >
-           close
-          </button>
-          {/* <button>Continue</button> */}
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Message;
