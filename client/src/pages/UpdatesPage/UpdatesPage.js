3594add6481364f022703f32b597979464405457
abdumamdouh <eabdogfx@gmail.com>
2022-03-11 22:19:56 +0200

    updates layout

--- client/src/pages/UpdatesPage/UpdatesPage.js
+++ client/src/pages/UpdatesPage/UpdatesPage.js
@@ -4,14 +4,43 @@ import "./UpdatePage.scss";
 
 const UPDATES = [
     {
-        examTopic: "zzz"
+        examTopic: "nigga",
+        recruiter: "elsisi",
+        jobTitile: "zbal",
+        img: "https://a.allegroimg.com/original/115895/b594fa094f3288495c442ac555f5/KLOCKI-HAM-BMW-T-E60-E61-VALEO-Typ-samochodu-Samochody-osobowe"
     }
 ];
 
 const UpdatesPage = (props) => {
     return (
-        <div className="container">
-            <span>hello world</span>
+        <div className="c">
+            <h3 className="hh3">Your Updates</h3>
+            <div className="updates">
+                <div className="update">
+                    <img
+                        src="https://a.allegroimg.com/original/115895/b594fa094f3288495c442ac555f5/KLOCKI-HAM-BMW-T-E60-E61-VALEO-Typ-samochodu-Samochody-osobowe"
+                        alt="logo"
+                        className="immg"
+                    />
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Explicabo in enim consequuntur incidunt iure non. Nisi,
+                        est iste. Sit, quibusdam!
+                    </p>
+                </div>
+                <div className="update">
+                    <img
+                        src="https://a.allegroimg.com/original/115895/b594fa094f3288495c442ac555f5/KLOCKI-HAM-BMW-T-E60-E61-VALEO-Typ-samochodu-Samochody-osobowe"
+                        alt="logo"
+                        className="immg"
+                    />
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Explicabo in enim consequuntur incidunt iure non. Nisi,
+                        est iste. Sit, quibusdam!
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
