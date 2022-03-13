26d4abb6b525f26c02399c9153f87661057c00e2
MarwanEmadMourad <marwanemad36@gmail.com>
2022-03-13 14:53:45 +0200

    task uploads db model.

---
+++ server/src/models/TaskUploads.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize')
+const db = require('../db/db')
+const Task = require('./Task')
+const Job = require('./Job')
+
+const TaskUploads = db.define('TaskUploads',{
+    id:{
+        type: Sequelize.INTEGER,
+        primaryKey: true ,
+        autoIncrement:true ,
+        allowNull: false
+    },
+    ApplicantId:{
+        type:Sequelize.INTEGER,
+        allowNull: false,
+        references:{
+            model:"Applicants",
+            key: "id"
+        }
+    },
+    uploadedTask:{
+        type:Sequelize.BLOB('long'),
+        allowNull:false
+    }
+});
+
+Job.belongsToMany(Task, { through: TaskUploads });
+Task.belongsToMany(Job, { through: TaskUploads });
+
+module.exports = TaskUploads ;
