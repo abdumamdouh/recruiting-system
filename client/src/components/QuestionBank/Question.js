3cb62a80ad80eae13ef6a8bc5510edb7affa3a7c
AntoineZakaria <tonyzakaria53@gmail.com>
2022-03-17 11:19:38 +0200

    importing styles

--- client/src/components/QuestionBank/Question.js
+++ client/src/components/QuestionBank/Question.js
@@ -1,12 +1,9 @@
 import React, {useState, useEffect} from "react";
-import classes from "./Feed.module.scss"
+import classes from "./QuestionBank.module.scss"
 import { useHistory} from 'react-router-dom'
-import {  useSelector } from "react-redux";
-import Button from "@mui/material/Button";
 
 const Question = ({question}) => {
     const history = useHistory()
-    //console.log(job)
     return(
         <div className={classes.job}>
  
