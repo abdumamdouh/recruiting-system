5da372a050375756650cd1f2754effbf6bd4fb81
AntoineZakaria <tonyzakaria53@gmail.com>
2022-03-17 11:49:10 +0200

    restructuring question

--- client/src/components/QuestionBank/Question.js
+++ client/src/components/QuestionBank/Question.js
@@ -3,14 +3,30 @@ import classes from "./QuestionBank.module.scss"
 import { useHistory} from 'react-router-dom'
 
 const Question = ({question}) => {
+    const answers=["answer1","answer2","answer3","answer4"]
+
+
     const history = useHistory()
     return(
-        <div className={classes.job}>
- 
-    
-            <div className={classes.jobInfo}>
-                
+        <div className={classes.question}>
+            <div className={classes.questionInfo}>
+                How old are you ?    
             </div>
+            
+                {answers.map(answer=>{
+                    return(
+                        <div>
+                    <input
+                        type="radio"
+                        name={answer}
+                        value={answer}/>
+                    <label>
+                    {answer}
+                    </label>        
+                    </div>
+                    )
+                })}
+            
 
         </div>
 
