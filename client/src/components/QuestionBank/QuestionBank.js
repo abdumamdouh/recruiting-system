f9f52eb5d04ead09c43557393e18bdd83a96cc57
AntoineZakaria <tonyzakaria53@gmail.com>
2022-03-17 12:31:22 +0200

    adding question bank

--- client/src/components/QuestionBank/QuestionBank.js
+++ client/src/components/QuestionBank/QuestionBank.js
@@ -2,37 +2,34 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Skeleton from "@mui/material/Skeleton";
 import Stack from "@mui/material/Stack";
-import JobPost from "./JobPost";
-import classes from "./Feed.module.scss";
+import classes from "./QuestionBank.module.scss"
+import Question from "./Question"
 import ReactPaginate from "react-paginate";
-import { getJobsAction } from "../../redux/actions/jobs";
 
-const Feed = () => {
+const QuestionBank = () => {
     const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getJobsAction(1));
-    }, [dispatch]);
+    // useEffect(() => {
+    //     dispatch(getJobsAction(1));
+    // }, [dispatch]);
 
     const Jobs = useSelector(state => state.jobs.Jobs);
     const { Count } = useSelector(state => state.jobs);
     //console.log('cc',Count)
     // const Count = 0;
-
+    const questions=[1,2,3,4,5,6,7,8,9,10]
     const [pageNumber, setPageNumber] = useState(1);
 
-    // const pagesVisited = pageNumber*jobsPerPage
 
-    const displayJobs = () => {};
     const changePage = ({ selected }) => {
         setPageNumber(selected + 1);
-        dispatch(getJobsAction(selected + 1));
+       // dispatch(getJobsAction(selected + 1));
     };
     if (Jobs !== undefined) {
         return (
             <div className={classes.list}>
-                {Jobs.map(job => (
-                    <JobPost job={job} />
+                {questions.map(question => (
+                    <Question question={question} />
                 ))}
                 <br />
                 <ReactPaginate
@@ -58,4 +55,4 @@ const Feed = () => {
         );
 };
 
-export default Feed;
+export default QuestionBank;
