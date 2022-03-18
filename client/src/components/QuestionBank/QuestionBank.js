8f839d5f77e6fa9a60ad32a97ec3e6b585e77580
Marwan-Alghandour <marwanalghandour23@gmail.com>
2022-03-18 02:55:30 +0200

    QuestionBank styling

--- client/src/components/QuestionBank/QuestionBank.js
+++ client/src/components/QuestionBank/QuestionBank.js
@@ -1,66 +1,206 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { DataGrid, GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
 import Skeleton from "@mui/material/Skeleton";
 import Stack from "@mui/material/Stack";
-import classes from "./QuestionBank.module.scss"
-import Question from "./Question"
+import classes from "./QuestionBank.module.scss";
+import Question from "./Question";
 import ReactPaginate from "react-paginate";
 // import { Formik, Form } from "formik";
 // import SelectWrapper from "../Forms/SelectWrapper";
 // import ButtonWrapper from "../Forms/ButtonWrapper";
 // import { Container, Grid, Typography } from "@material-ui/core";
 
-
 const QuestionBank = () => {
-    
     const dispatch = useDispatch();
 
     // useEffect(() => {
     //     dispatch(getJobsAction(1));
     // }, [dispatch]);
 
-    const Jobs = useSelector(state => state.jobs.Jobs);
-    const { Count } = useSelector(state => state.jobs);
+    const Jobs = useSelector((state) => state.jobs.Jobs);
+    const { Count } = useSelector((state) => state.jobs);
     const [pageNumber, setPageNumber] = useState(1);
-    const [view,setView]=useState('notsw');
+    const [view, setView] = useState("notsw");
 
-    
     const changePage = ({ selected }) => {
         setPageNumber(selected + 1);
-       // dispatch(getJobsAction(selected + 1));
+        // dispatch(getJobsAction(selected + 1));
     };
 
+    const questions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const columns = [
+        {
+            field: "category",
+            headerName: "Category",
+            width: 90,
+            sortable: false
+        },
+        {
+            field: "topic",
+            headerName: "Topic",
+            width: 150,
+            sortable: false
+        },
+        {
+            field: "question",
+            headerName: "Question",
+            width: 150,
+            sortable: false
+        },
+        {
+            field: "choices",
+            headerName: "Choices",
+            width: 110,
+            sortable: false,
+            renderCell: (params) => (
+                // console.log(params.value);
+                <ul>
+                    {params.value.map((choice) => (
+                        <li>{choice}</li>
+                    ))}
+                </ul>
+            )
+        },
+        {
+            field: "answer",
+            headerName: "Answer",
+            description: "This column has a value getter and is not sortable.",
+            width: 160,
+            sortable: false
+        },
+        {
+            field: "difficulty",
+            headerName: "Difficulty",
+            sortable: true,
+            sortComparator: 
+            width: 160,
+            renderCell: (params) => {
+                // console.log(value);
+                if (params.value === "Easy") {
+                    return (
+                        <span style={{ color: "#027D6F" }}>{params.value}</span>
+                    );
+                } else if (params.value === "Medium") {
+                    return (
+                        <span style={{ color: "#FFC01E" }}>{params.value}</span>
+                    );
+                } else if (params.value === "Hard") {
+                    return (
+                        <span style={{ color: "#FF375F" }}>{params.value}</span>
+                    );
+                } else {
+                    return <span>{params.value}</span>;
+                }
+            }
+        }
+    ];
 
+    const rows = [
+        {
+            internalId: 1,
+            category: "s",
+            topic: "Snow",
+            question: "Jon",
+            choices: ["dsd", "sdd", "fdfd", "ffgf"],
+            answer: "dsd",
+            difficulty: "Easy"
+        },
+        {
+            internalId: 2,
+            category: "s",
+            topic: "Snow",
+            question: "Jon",
+            choices: ["dsd", "sdd", "fdfd", "ffgf"],
+            answer: "dsd",
+            difficulty: "Medium"
+        },
+        {
+            internalId: 3,
+            category: "s",
+            topic: "Snow",
+            question: "Jon",
+            choices: ["dsd", "sdd", "fdfd", "ffgf"],
+            answer: "dsd",
+            difficulty: "Hard"
+        },
+        {
+            internalId: 4,
+            category: "s",
+            topic: "Snow",
+            question: "Jon",
+            choices: ["dsd", "sdd", "fdfd", "ffgf"],
+            answer: "dsd",
+            difficulty: "Not specified"
+        },
+        {
+            internalId: 5,
+            category: "s",
+            topic: "Snow",
+            question: "Jon",
+            choices: ["dsd", "sdd", "fdfd", "ffgf"],
+            answer: "dsd",
+            difficulty: "Easy"
+        },
+        {
+            internalId: 6,
+            category: "s",
+            topic: "Snow",
+            question: "Jon",
+            choices: ["dsd", "sdd", "fdfd", "ffgf"],
+            answer: "dsd",
+            difficulty: "Easy"
+        },
+        {
+            internalId: 7,
+            category: "s",
+            topic: "Snow",
+            question: "Jon",
+            choices: ["dsd", "sdd", "fdfd", "ffgf"],
+            answer: "dsd",
+            difficulty: "Easy"
+        },
+        {
+            internalId: 8,
+            category: "s",
+            topic: "Snow",
+            question: "Jon",
+            choices: ["dsd", "sdd", "fdfd", "ffgf"],
+            answer: "dsd",
+            difficulty: "Easy"
+        },
+        {
+            internalId: 9,
+            category: "s",
+            topic: "Snow",
+            question: "Jon",
+            choices: ["dsd", "sdd", "fdfd", "ffgf"],
+            answer: "dsd",
+            difficulty: "Easy"
+        }
+    ];
+    const filters = [
+        { value: "software", label: "software" },
+        { value: "medicine", label: "medicine" },
+        { value: "Education", label: "Education" }
+    ];
+    const secondFilter = ["java", "C++"];
 
+    const filtervalues = {
+        category: "",
+        topic: ""
+    };
 
+    const handleView = (e) => {
+        setView(e.target.value);
+    };
 
-    const questions=[1,2,3,4,5,6,7,8,9,10]
-    const filters=[   { value: "software", label: "software" },
-    { value: "medicine", label: "medicine" },
-    { value: "Education", label: "Education" },
-   ]
-    const secondFilter=["java","C++"]
-
-
-    const filtervalues={
-        category:'',
-        topic:''
-    }
-
-  const  handleView =(e)=>{
-      setView(e.target.value)
-
-  }
-
-  const bringquestions =()=>{
-      
-  }
+    const bringquestions = () => {};
 
     if (questions !== undefined) {
         return (
-
-            <div>
-{/* 
+            <div style={{ height: 400 }}>
+                {/* 
                 <div>
 
                 <Formik
@@ -95,17 +235,20 @@ const QuestionBank = () => {
 
                 </div> */}
 
+                <DataGrid
+                    className="MuiDataGrid"
+                    rows={rows}
+                    getRowId={(row) => row.internalId}
+                    rowHeight={rows[0].choices.length * 25}
+                    columns={columns}
+                    pageSize={10}
+                    disableColumnSelector={true}
+                    sx={{
+                        "& .MuiDataGrid-cell:focus-within": { outline: "none" }
+                    }}
+                />
 
-
-
-
-
-
-
-
-
-
-                        <lable>
+                {/* <lable>
                             Category
                         </lable>
                         <select
@@ -164,8 +307,8 @@ const QuestionBank = () => {
                         disabledClassName={classes.paginationDisabled}
                         activeClassName={classes.paginationActive}
                         />
-                </div>
-             </div>
+                </div> */}
+            </div>
         );
     } else
         return (
