a1c9860b2c0194210248b5214238dd899ffac3cd
Mahmoudnaoum <mahmoudnaoum13@gmail.com>
2022-03-20 23:50:19 +0200

    question bank

--- client/src/redux/reducers/bank.js
+++ client/src/redux/reducers/bank.js
@@ -21,8 +21,8 @@ const bankReducer = (state = {}, action) => {
 
         case CHOOSE_CATEGORY_SUCCESS:
             return {
-                category:action.payload,    
-                ...state}
+                category:action.payload  
+            }
 
         case CHOOSE_CATEGORY_FAIL:
             return{
@@ -66,7 +66,7 @@ const bankReducer = (state = {}, action) => {
                 error: action.payload,
             }    
         default:
-            return state;
+            return {};
     }
 };
 export default bankReducer;
