12df23eb438442832cc56522591a04fa243d2148
Marwan-Alghandour <marwanalghandour23@gmail.com>
2022-04-24 14:54:07 +0200

    Fixing and styling countdown timer

--- client/src/pages/McqTestPage/MyTimer.js
+++ client/src/pages/McqTestPage/MyTimer.js
@@ -1,42 +1,37 @@
 import React from "react";
 import { useTimer } from "react-timer-hook";
+import "./MyTimer.scss";
 
 function MyTimer({ expiryTimestamp }) {
-    const {
-        seconds,
-        minutes,
-        hours,
-        days,
-        isRunning,
-        start,
-        pause,
-        resume,
-        restart
-    } = useTimer({
+    const { seconds, minutes, hours } = useTimer({
         expiryTimestamp,
         onExpire: () => console.warn("onExpire called")
     });
 
     return (
-        <div style={{ textAlign: "center" }}>
-            <div style={{ fontSize: "100px" }}>
-                <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:
-                <span>{seconds}</span>
-            </div>
-            <p>{isRunning ? "Running" : "Not running"}</p>
-            <button onClick={start}>Start</button>
-            <button onClick={pause}>Pause</button>
-            <button onClick={resume}>Resume</button>
-            <button
-                onClick={() => {
-                    // Restarts to 5 minutes timer
-                    const time = new Date();
-                    time.setSeconds(time.getSeconds() + 300);
-                    restart(time);
+        <div
+            style={{
+                textAlign: "center",
+                position: "fixed",
+                right: "0%",
+                bottom: "0%",
+                margin: "10px"
+            }}
+        >
+            <small>Remaining Time</small>
+            <div
+                style={{
+                    border: "2px solid",
+                    padding: "0.2rem",
+                    fontSize: "20px"
                 }}
             >
-                Restart
-            </button>
+                <div className={minutes < 10 && "blink"}>
+                    <span>{String(hours).padStart(2, "0")}</span>:
+                    <span>{String(minutes).padStart(2, "0")}</span>:
+                    <span>{String(seconds).padStart(2, "0")}</span>
+                </div>
+            </div>
         </div>
     );
 }
