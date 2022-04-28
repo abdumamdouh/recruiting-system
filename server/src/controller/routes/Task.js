9c6964f63871f9291114a94c9c45d599d8f12853
Marwan-Alghandour <marwanalghandour23@gmail.com>
2022-04-29 01:47:36 +0200

    stringify the assignedObj

--- server/src/controller/routes/Task.js
+++ server/src/controller/routes/Task.js
@@ -90,7 +90,7 @@ router.post(
                 throw new Error("You did not apply for this job");
             }
             assignedObj = JSON.parse(assigned.assigned);
-            
+
             //\"2\"
             if (!assignedObj.tasks.includes(req.params.TaskId)) {
                 throw new Error("You are not assigned this task.");
@@ -102,17 +102,22 @@ router.post(
                 ApplicantId: req.applicant.id
             });
 
-            // removing the task id from the assigned tasks after submission 
-            const deletedIndex = assignedObj.tasks.indexOf(req.params.TaskId.toString())
-            assignedObj.tasks.splice(deletedIndex,1)
-            await ApplyFor.update({
-                assigned:assignedObj
-            },{
-                where:{
-                    JobId: req.params.JobId,
-                    ApplicantId: req.applicant.id
+            // removing the task id from the assigned tasks after submission
+            const deletedIndex = assignedObj.tasks.indexOf(
+                req.params.TaskId.toString()
+            );
+            assignedObj.tasks.splice(deletedIndex, 1);
+            await ApplyFor.update(
+                {
+                    assigned: JSON.stringify(assignedObj)
+                },
+                {
+                    where: {
+                        JobId: req.params.JobId,
+                        ApplicantId: req.applicant.id
+                    }
                 }
-            })
+            );
             res.status(201).send("Task uploaded successfully.");
         } catch (error) {
             // console.log(error);
@@ -130,48 +135,50 @@ router.post(
 
 // recruiter can see all of a certain task details
 // Accepted JSON: None
-router.get(
-    "*/:JobId/:TaskId/taskDetails",
-    recruiterAuth,
-    async (req, res) => {
-        try {
-            // checking if you're the one who posted this job or not
-            const result = await Job.findOne({
-                attributes: ["RecruiterId"],
-                where: {
-                    id: req.params.JobId
-                },
-                raw: true
-            });
-            if (req.recruiter.id !== result.RecruiterId) {
-                throw new Error("You are not authorized to view this job");
-            }
-            // getting all tasks uploaded by the applicants
-            const solutions = await TaskUploads.findAll({
-                attributes: ["ApplicantId", "uploadedTask", "createdAt","score","feedback"],
-                where: {
-                    JobId: req.params.JobId,
-                    TaskId: req.params.TaskId
-                }
-            });
-            for(let i = 0 ; i <solutions.length ; i++){
-                if (Buffer.byteLength(solutions[i].uploadedTask)) {
-                    const buffer = solutions[i].uploadedTask ;
-                    solutions[i].dataValues.type = fileType(buffer); 
-                }
-                solutions[i].dataValues.applicantName = await Applicant.findOne({
-                    attributes: ["firstName","lastName"],
-                    where:{
-                        id:solutions[i].ApplicantId
-                    }
-                })
-            }
-            res.send(solutions);
-        } catch (error) {
-            res.status(400).send(error.message);
+router.get("*/:JobId/:TaskId/taskDetails", recruiterAuth, async (req, res) => {
+    try {
+        // checking if you're the one who posted this job or not
+        const result = await Job.findOne({
+            attributes: ["RecruiterId"],
+            where: {
+                id: req.params.JobId
+            },
+            raw: true
+        });
+        if (req.recruiter.id !== result.RecruiterId) {
+            throw new Error("You are not authorized to view this job");
         }
+        // getting all tasks uploaded by the applicants
+        const solutions = await TaskUploads.findAll({
+            attributes: [
+                "ApplicantId",
+                "uploadedTask",
+                "createdAt",
+                "score",
+                "feedback"
+            ],
+            where: {
+                JobId: req.params.JobId,
+                TaskId: req.params.TaskId
+            }
+        });
+        for (let i = 0; i < solutions.length; i++) {
+            if (Buffer.byteLength(solutions[i].uploadedTask)) {
+                const buffer = solutions[i].uploadedTask;
+                solutions[i].dataValues.type = fileType(buffer);
+            }
+            solutions[i].dataValues.applicantName = await Applicant.findOne({
+                attributes: ["firstName", "lastName"],
+                where: {
+                    id: solutions[i].ApplicantId
+                }
+            });
+        }
+        res.send(solutions);
+    } catch (error) {
+        res.status(400).send(error.message);
     }
-);
+});
 // retruned JSON:
 // [
 //     {
