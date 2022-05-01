b5bcec2f75d7c043d92b214cc9b88d0bf3820901
AntoineZakaria <tonyzakaria53@gmail.com>
2022-05-01 15:48:23 +0200

    adding action for get cp

---
+++ client/src/redux/actions/codingProblemBank.js
@@ -0,0 +1,35 @@
+import {
+    GET_CODINGPROBLEMS_REQUEST,
+    GET_CODINGPROBLEMS_SUCCESS,
+    GET_CODINGPROBLEMS_FAIL,
+} from "../types/index";
+export const getCodingProblemsAction = (pageNumber) => {
+    return async (dispatch, getState) => {
+        try {
+            dispatch({
+                type: GET_CODINGPROBLEMS_REQUEST,
+                loading: true
+            });
+            const { userInfo } = getState().user;
+            console.log(userInfo.token);
+            const rawResponse = await fetch(
+                `${serverURL}/getAllMCQs/${pageNumber}`,
+                {
+                    method: "GET",
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                        Authorization: "Bearer " + userInfo.token
+                    }
+                }
+            );
+            const data = await rawResponse.json();
+            dispatch({ type: GET_CODINGPROBLEMS_SUCCESS, payload: data });
+        } catch (error) {
+            dispatch({
+                type: GET_CODINGPROBLEMS_FAIL,
+                payload: error.response && error.response.data
+            });
+        }
+    };
+};
