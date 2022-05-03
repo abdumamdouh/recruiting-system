dc41e4ecb45a166836d954f9bf7708d4bdcdd35f
Marwan-Alghandour <marwanalghandour23@gmail.com>
2022-05-03 05:11:42 +0200

    Changing relation method with sequlize method to use through

--- server/src/models/ActiveCodingProbelms.js
+++ server/src/models/ActiveCodingProbelms.js
@@ -1,36 +1,47 @@
-const Sequelize = require('sequelize')
-const db = require('../db/db')
-// const CodingProblemBank = require("./CodingProblemBank")
-// const Job = require("./Job")
+const Sequelize = require("sequelize");
+const db = require("../db/db");
+const CodingProblemBank = require("./CodingProblemBank");
+const Job = require("./Job");
 
-
-const ActiveCodingProblem = db.define('ActiveCodingProblem',{
-    codingProblemId:{
-        type:Sequelize.INTEGER,
-        allowNull:false,
-        primaryKey:true,
-        references: {
-            model: 'CodingProblemBanks', 
-            key: 'id'
-        },
+const ActiveCodingProblem = db.define("ActiveCodingProblem", {
+    // codingProblemId:{
+    //     type:Sequelize.INTEGER,
+    //     allowNull:false,
+    //     primaryKey:true,
+    //     references: {
+    //         model: 'CodingProblemBanks',
+    //         key: 'id'
+    //     },
+    // },
+    // jobId:{
+    //     type:Sequelize.INTEGER,
+    //     allowNull:false,
+    //     primaryKey:true,
+    //     references: {
+    //         model: 'Jobs',
+    //         key: 'id'
+    //     },
+    // },
+    deadline: {
+        type: Sequelize.DATE,
+        allowNull: false
     },
-    jobId:{
-        type:Sequelize.INTEGER,
-        allowNull:false,
-        primaryKey:true,
-        references: {
-            model: 'Jobs', 
-            key: 'id'
-        },
-    },
-    deadline:{
-        type:Sequelize.DATE,
-        allowNull:false
-    },
-    duration:{
-        type:Sequelize.INTEGER,
-        allowNull:false
+    duration: {
+        type: Sequelize.INTEGER,
+        allowNull: false
     }
+});
+
+// M->N relation between Job and task
+Job.belongsToMany(CodingProblemBank, {
+    foreignKey: "codingProblemId",
+    otherKey: "jobId",
+    through: ActiveCodingProblem
+});
+CodingProblemBank.belongsToMany(Job, {
+    through: ActiveCodingProblem,
+    foreignKey: "jobId",
+    otherKey: "codingProblemId"
 });
 
 module.exports = ActiveCodingProblem;
