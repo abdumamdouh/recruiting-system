a6056a4b06e9f71399dae5086883e3e65ea6a8cd
Marwan-Alghandour <marwanalghandour23@gmail.com>
2022-07-04 02:44:30 +0200

    Adding the average chart to each card

--- client/src/components/ResultCard/ResultCard.js
+++ client/src/components/ResultCard/ResultCard.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import GaugeChart from "react-gauge-chart";
 import ResultPopup from "./ResultPopup";
-function ResultCard({ title, values }) {
+function ResultCard({ title, avg, values }) {
     const history = useHistory();
     const [openResult, setOpenResult] = useState(false);
     const showResults = () => {
@@ -14,8 +15,35 @@ function ResultCard({ title, values }) {
             <div class="card" style={{ marginBottom: "10px" }}>
                 <div class="card-header">{title}</div>
                 <div class="card-body">
-                    {/* <h5 class="card-title">{title}</h5> */}
-                    <p class="card-text">View applicants' results</p>
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            alignContent: "center",
+                            justifyContent: "center"
+                        }}
+                    >
+                        <h5 class="card-title">Average Score</h5>
+                        <GaugeChart
+                            id="gauge-chart"
+                            textColor="#333"
+                            nrOfLevels={3}
+                            arcsLength={[0.5, 0.3, 0.2]}
+                            colors={["#EA4228", "#F5CD19", "#5BE12C"]}
+                            needleColor="#999"
+                            percent={Number(avg) / 100}
+                            arcPadding={0.02}
+                            hideText
+                        />
+                        <span
+                            style={{
+                                fill: "rgb(51, 51, 51)",
+                                fontSize: "24.5455px",
+                                alignSelf: "center"
+                            }}
+                        >{`${Number(avg)}/100`}</span>
+                    </div>
+                    {/* <p class="card-text">View applicants' results</p> */}
 
                     <div>
                         <button
