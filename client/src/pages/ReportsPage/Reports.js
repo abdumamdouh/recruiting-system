64bd7f71da8b103b0878f8b0af49136c2f88ce77
salmaehab <salmaiihab@gmail.com>
2022-07-05 01:22:28 +0200

    Update reports layout

--- client/src/pages/ReportsPage/Reports.js
+++ client/src/pages/ReportsPage/Reports.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-import ResultCard from "../../components/ResultCard/ResultCard";
-import { getJobResultsAction } from "../../redux/actions/jobs";
+
 import Skeleton from "@mui/material/Skeleton";
 import Stack from "@mui/material/Stack";
 import Slider from "react-slick";
+import ResultCard from "../../components/ResultCard/ResultCard";
+import { getJobResultsAction } from "../../redux/actions/jobs";
 import {
     BarChart,
     Bar,
@@ -20,49 +21,49 @@ import {
 function SampleNextArrow(props) {
     const { className, style, onClick } = props;
     return (
-      <div
-        className={className}
-        style={{ ...style, display: "block", background: "grey" }}
-        onClick={onClick}
-      />
+        <div
+            className={className}
+            style={{ ...style, display: "block", background: "grey" }}
+            onClick={onClick}
+        />
     );
-  }
-  
-  function SamplePrevArrow(props) {
+}
+
+function SamplePrevArrow(props) {
     const { className, style, onClick } = props;
     return (
-      <div
-        className={className}
-        style={{ ...style, display: "block", background: "grey" }}
-        onClick={onClick}
-      />
+        <div
+            className={className}
+            style={{ ...style, display: "block", background: "grey" }}
+            onClick={onClick}
+        />
     );
-  }
+}
 const Reports = () => {
-    
     const settings = {
         dots: true,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
+        
         // nextArrow: <SampleNextArrow />,
         // prevArrow: <SamplePrevArrow />,
         swipeToSlide: true,
-      };
+        className: 'slides'
+    };
     const [resultstate, setResults] = useState({});
     const [mcqResult, setMcqResult] = useState([]);
     const [taskResult, setTaskResult] = useState([]);
     const [codingResult, setCodingResult] = useState([]);
     const [loading, setLoading] = useState(false);
-    const state = useSelector((state) => state);
+    const state = useSelector(state => state);
     const { id } = state.job;
     const dispatch = useDispatch();
-    const results = useSelector((state) => state.results);
+    const results = useSelector(state => state.results);
     useEffect(() => {
-        console.log('aaa')
         dispatch(getJobResultsAction(id, setLoading));
-        console.log(loading);
+
         if (loading == true) {
             setValues();
         }
@@ -70,8 +71,7 @@ const Reports = () => {
     const M = results.mcqsResults;
     const T = results.tasksResults;
     const C = results.codingProblemsResults;
-    
-    
+
     const renderCustomBarLabel = ({ payload, x, y, width, height, value }) => {
         return (
             <text
@@ -93,9 +93,7 @@ const Reports = () => {
         for (const [key, value] of Object.entries(T)) {
             setTaskResult([...[value]]);
         }
-        
     };
-    
 
     if (results !== undefined && loading == true) {
         const data = results.overallScore.map(
@@ -108,12 +106,12 @@ const Reports = () => {
         );
         console.log(results.overallScore[0].overallScore + 200);
         return (
-            <div>
-            <div
+            <div style={{marginTop: '40px', marginBottom: '60px'}}>
+                <div
                     style={{
                         display: "flex",
                         alignSelf: "center",
-                        flexDirection: "column"
+                        flexDirection: "column",
                     }}
                 >
                     <h5 style={{ marginLeft: "2rem" }} class="card-title">
@@ -143,10 +141,11 @@ const Reports = () => {
                         </BarChart>
                     </ResponsiveContainer>
                 </div>
-            <Slider {...settings}>
-            <div>
+                <div > 
+                <Slider {...settings}>
+                    <div>
                         <br />
-                        <h4>MCQ results</h4>
+                        <h4>MCQ </h4>
                         {mcqResult.map((item, index) => (
                             <ResultCard
                                 key={index}
@@ -160,7 +159,7 @@ const Reports = () => {
                         ))}
                     </div>
                     <div>
-                        <h4>Coding problems results</h4>
+                        <h4>Coding problems</h4>
                         <div>
                             {codingResult.map((item, index) => (
                                 <ResultCard
@@ -177,7 +176,7 @@ const Reports = () => {
                         <br />
                     </div>
                     <div>
-                        <h4>Task results</h4>
+                        <h4>Task </h4>
                         <div>
                             {taskResult.map((item, index) => (
                                 <ResultCard
@@ -192,102 +191,11 @@ const Reports = () => {
                             ))}
                         </div>
                     </div>
+                </Slider>
+                </div>
+                
+                </div>
             
-          </Slider>
-            <div
-                style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    alignContent: "center"
-                }}
-            >
-                <div>
-                    {" "}
-                    {/* <div>
-                        <br />
-                        <h4>MCQ results</h4>
-                        {mcqResult.map((item, index) => (
-                            <ResultCard
-                                key={index}
-                                title={item.title}
-                                avg={
-                                    results.avgMCQsScore[index]
-                                        .average_MCQ_score
-                                }
-                                values={item.values}
-                            />
-                        ))}
-                    </div> */}
-                    {/* <div>
-                        <h4>Coding problems results</h4>
-                        <div>
-                            {codingResult.map((item, index) => (
-                                <ResultCard
-                                    key={index}
-                                    title={item.title}
-                                    avg={
-                                        results.avgCodingProblemsScore[index]
-                                            .average_CodingProblem_score
-                                    }
-                                    values={item.values}
-                                />
-                            ))}
-                        </div>
-                        <br />
-                    </div> */}
-                    {/* <div>
-                        <h4>Task results</h4>
-                        <div>
-                            {taskResult.map((item, index) => (
-                                <ResultCard
-                                    key={index}
-                                    title={item.title}
-                                    avg={
-                                        results.avgTasksScore[index]
-                                            .average_Task_score
-                                    }
-                                    values={item.values}
-                                />
-                            ))}
-                        </div>
-                    </div> */}
-                </div>
-                {/* <div
-                    style={{
-                        display: "flex",
-                        alignSelf: "center",
-                        flexDirection: "column"
-                    }}
-                >
-                    <h5 style={{ marginLeft: "2rem" }} class="card-title">
-                        Overall Score
-                    </h5>
-                    <ResponsiveContainer width={600} height={400}>
-                        <BarChart width={150} height={40} data={data}>
-                            <XAxis dataKey="name" />
-                            <YAxis
-                                domain={[
-                                    0,
-                                    Math.ceil(
-                                        results.overallScore[0].overallScore /
-                                            10
-                                    ) *
-                                        10 +
-                                        20
-                                ]}
-                            />
-                            <Bar
-                                dataKey="uv"
-                                barSize={40}
-                                fill="#8884d8"
-                                isAnimationActive={false}
-                                label={renderCustomBarLabel}
-                            />
-                        </BarChart>
-                    </ResponsiveContainer>
-                </div> */}
-            </div>
-            </div>
         );
     } else
         return (
